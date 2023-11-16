755afba896905f47283a8a94543d1e65eadd1097
Qazi1709 <hassansiddiqui1709@gmail.com>
2023-11-16 22:20:19 +0530

    Twilio SMS Service

--- server/utils/sendSms.js
+++ server/utils/sendSms.js
@@ -1,6 +1,6 @@
 import twilio from 'twilio';
-const accountSid = 'ACb11a4a31b9efb9d78245f332d49cc8d6';
-const authToken = '55ddc5b48c702cabaa5daef797f40644';
+const accountSid = '';
+const authToken = '';
 
 const client = new twilio(accountSid, authToken);
 
@@ -9,7 +9,7 @@ async function sendSMS() {
         let message = await client.messages
             .create({
                 body: 'Dear customer,this is your verification code to log in to your account.This code Expires in next 5 mins.',
-                from: '+16562188446',
+                from: '+',
                 to: '+918919142747'
             })
         console.log(message.sid);
